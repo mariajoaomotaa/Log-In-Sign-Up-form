47ddc3bcea28e58e65e60bb57bbb0dc6879fe14d
Maria João <142997408+mariajoaomotaa@users.noreply.github.com>
2023-11-12 15:48:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const form = document.getElementById('form');
 const errorElement1 = document.getElementById('error1');
 const errorElement2 = document.getElementById('error2');
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-const userRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+const userRegex = /^[a-zA-Z0-9]+$/;
 
 form.addEventListener('submit', (e) => {
     //Validação da password//
@@ -23,11 +23,46 @@ form.addEventListener('submit', (e) => {
     let messages2 = []
 
     if (!userRegex.test(user.value)) {
-        messages2.push("Username must not include special characters or spaces.");
+        messages2.push("Username must not include special characters or spaces");
     };
 
     if (messages2.length > 0) {
         e.preventDefault();
         errorElement2.innerText = messages2.join(', ');
     }
+
+    if (messages1.length === 0 && messages2.length === 0) {
+        showModal();
+    }
 })
+
+document.getElementById('icon').addEventListener('mousedown', function() {
+    document.getElementById('password').type = 'text';
+});
+
+document.getElementById('icon').addEventListener('mouseup', function() {
+    document.getElementById('password').type = 'password';
+});
+
+
+function showModal() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+
+    const okClose = document.getElementById('okClose');
+    const loginButton = document.getElementById('loginButton');
+
+    okClose.onclick = function () {
+        modal.style.display = 'none';
+    };
+
+    loginButton.onclick = function () {
+        window.location.href = 'login.html';
+    };
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    };
+}
